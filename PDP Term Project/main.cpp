4b34215d981728cddc6cea26d4f24ad2b2af8a60
Po-Chuan Wang <r02246011@ntu.edu.tw>
2014-06-04 23:50:08 +0800

    Initial Commit

---
+++ PDP Term Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PDP Term Project
+//
+//  Created by Po-Chuan Wang on 2014/6/4.
+//  Copyright (c) 2014年 COLA Lab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
