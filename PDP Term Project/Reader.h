c4d1f717274921deafb2b69f512540ff3d18bb3f
Po-Chuan Wang <r02246011@ntu.edu.tw>
2014-06-05 00:01:41 +0800

    Initial commit from Xcode

---
+++ PDP Term Project/Reader.h
@@ -0,0 +1,17 @@
+//
+//  Reader.h
+//  PDP Term Project
+//
+//  Created by Po-Chuan Wang on 2014/6/4.
+//  Copyright (c) 2014年 COLA Lab. All rights reserved.
+//
+
+#ifndef PDP_Term_Project_Reader_h
+#define PDP_Term_Project_Reader_h
+
+class AbstractReader {
+public:
+    virtual void read(const char *filename) = 0;
+};
+
+#endif
